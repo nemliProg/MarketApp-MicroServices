51b87d22c37b403e2cb012493d870774d817c841
nemliProg <younesbuzz@gmail.com>
2023-04-26 18:12:04 +0100

    add CRUD Endpoints in the Product controller

--- product-service/src/main/java/ma/codipster/productservice/controller/ProductController.java
+++ product-service/src/main/java/ma/codipster/productservice/controller/ProductController.java
@@ -1,11 +1,41 @@
 package ma.codipster.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import ma.codipster.productservice.dto.product.ProductRequest;
+import ma.codipster.productservice.dto.product.ProductResponse;
+import ma.codipster.productservice.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
 public class ProductController {
 
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable String id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@RequestBody ProductRequest productRequest, @PathVariable String id){
+        productService.updateProduct(productRequest, id);
+        return ResponseEntity.ok().build();
+    }
 
 }
