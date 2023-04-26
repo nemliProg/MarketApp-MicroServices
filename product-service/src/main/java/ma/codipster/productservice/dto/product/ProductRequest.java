ba645c7a3e1fca0ae50a0451d937b349f072f3e0
nemliProg <younesbuzz@gmail.com>
2023-04-26 14:01:01 +0100

    add the product Request Dto

---
+++ product-service/src/main/java/ma/codipster/productservice/dto/product/ProductRequest.java
@@ -0,0 +1,14 @@
+package ma.codipster.productservice.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String name;
+    private String description;
+    private String price;
+}
