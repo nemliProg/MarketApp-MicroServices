c57aab9f033d08e602a17f12c5ae8a4d22d4f963
nemliProg <younesbuzz@gmail.com>
2023-04-26 17:05:08 +0100

    change the price type

--- product-service/src/main/java/ma/codipster/productservice/dto/product/ProductRequest.java
+++ product-service/src/main/java/ma/codipster/productservice/dto/product/ProductRequest.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductRequest {
+public class ProductRequest{
     private String name;
     private String description;
-    private String price;
+    private BigDecimal price;
 }
--- product-service/src/main/java/ma/codipster/productservice/dto/product/ProductResponse.java
+++ product-service/src/main/java/ma/codipster/productservice/dto/product/ProductResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +13,5 @@ public class ProductResponse {
     private String id;
     private String name;
     private String description;
-    private String price;
+    private BigDecimal price;
 }
