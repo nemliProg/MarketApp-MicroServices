000a65e518e85acf0f568539bf9107c7f8fceb6a
nemliProg <younesbuzz@gmail.com>
2023-04-26 14:01:47 +0100

    add the product Controller

---
+++ product-service/src/main/java/ma/codipster/productservice/controller/ProductController.java
@@ -0,0 +1,11 @@
+package ma.codipster.productservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+
+}
