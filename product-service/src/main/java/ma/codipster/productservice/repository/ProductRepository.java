9abd1f8904c5c2903a9d24f0121f865d4935c39e
nemliProg <younesbuzz@gmail.com>
2023-04-26 14:00:03 +0100

    add the product Repository

---
+++ product-service/src/main/java/ma/codipster/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package ma.codipster.productservice.repository;
+
+import ma.codipster.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product,String> {
+}
