61c686affef9fee1f93559f1fe7f064317eb89b8
NEMLI <y.nemli@itroadgroup.com>
2023-05-11 14:35:33 +0100

    add orderRequest dto to handle order request object

---
+++ order-service/src/main/java/ma/codipster/orderservice/dto/order/OrderRequest.java
@@ -0,0 +1,17 @@
+package ma.codipster.orderservice.dto.order;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.codipster.orderservice.model.OrderLineItems;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItems> orderLineItemsList;
+}
