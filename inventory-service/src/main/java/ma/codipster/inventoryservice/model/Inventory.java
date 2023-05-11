eae798f4df2446526284c1020b9b29e14ffd11b9
NEMLI <y.nemli@itroadgroup.com>
2023-05-11 17:53:09 +0100

    add Inventory entity

---
+++ inventory-service/src/main/java/ma/codipster/inventoryservice/model/Inventory.java
@@ -0,0 +1,24 @@
+package ma.codipster.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String skuCode;
+
+    private Integer quantity;
+}
