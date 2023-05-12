b07a75a2f94a83c4080a59c7a851d50f4f86ecde
NEMLI <y.nemli@itroadgroup.com>
2023-05-12 09:02:59 +0100

    add Inventory controller

---
+++ inventory-service/src/main/java/ma/codipster/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,11 @@
+package ma.codipster.inventoryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+}
