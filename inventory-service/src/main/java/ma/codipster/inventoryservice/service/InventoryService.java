421f8913c575ae0b993bbe8139008100ae01f8fd
NEMLI <y.nemli@itroadgroup.com>
2023-05-12 09:46:13 +0100

    Add isInStock method to check inventory availability by SKU code

--- inventory-service/src/main/java/ma/codipster/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/ma/codipster/inventoryservice/service/InventoryService.java
@@ -1,9 +1,19 @@
 package ma.codipster.inventoryservice.service;
 
+import lombok.RequiredArgsConstructor;
+import ma.codipster.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
 }
