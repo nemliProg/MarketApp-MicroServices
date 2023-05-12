677b82adb2e630878682eb0d30bae448969e7295
NEMLI <y.nemli@itroadgroup.com>
2023-05-12 09:49:25 +0100

    Add GET endpoint to check inventory availability by SKU code

--- inventory-service/src/main/java/ma/codipster/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/ma/codipster/inventoryservice/controller/InventoryController.java
@@ -1,6 +1,10 @@
 package ma.codipster.inventoryservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import ma.codipster.inventoryservice.service.InventoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    public ResponseEntity<Boolean> isInStock(@PathVariable("sku-code") String skuCode){
+        return ResponseEntity.ok(inventoryService.isInStock(skuCode));
+    }
 }
