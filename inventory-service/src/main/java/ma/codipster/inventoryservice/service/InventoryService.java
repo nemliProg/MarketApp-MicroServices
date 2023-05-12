638a31d0f185921638978e5f607784bf72f258e9
NEMLI <y.nemli@itroadgroup.com>
2023-05-12 09:23:08 +0100

    add Inventory service

---
+++ inventory-service/src/main/java/ma/codipster/inventoryservice/service/InventoryService.java
@@ -0,0 +1,9 @@
+package ma.codipster.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class InventoryService {
+}
